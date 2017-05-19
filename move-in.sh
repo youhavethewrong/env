19415806ec24d30664d12f222f1fc0c528c094b6
ESC <youhavethewrong@gmail.com>
2017-05-18 21:38:17 -0500

    Add a little color to ps1

---
+++ move-in.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo apt-get install git emacs build-essential chromium-browser k3b clamav fortune-mod gimp vlc vlc-plugin-samba htop nmap netcat-openbsd openjdk-8-jre p7zip redshift-gtk remmina smbclient tmux transmission-gtk tree rar unrar unzip zip 
+
+git clone git@github:youhavethewrong/emacs.d.git ~/.emacs.d 
